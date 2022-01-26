43f98efe94769b9d5116fe0dd192f5b8dbc352b8
Andy Tzanidakis <atzanida@caltech.edu>
2022-01-25 23:01:06 -0800

    known error

--- ps1/_thermo_particles.py
+++ ps1/_thermo_particles.py
@@ -4,6 +4,7 @@ import pygame
 import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
+import random
 
 import matplotlib.pyplot as plt
 pygame.font.init()
@@ -16,7 +17,7 @@ width, height = 1000, 1000
 WIN = pygame.display.set_mode((width, height)) 
 CLR = (255, 255, 255)
 PRT_CLR = (0, 1, 0)
-FPS = 30
+FPS = 60
 
 
 class Particle():
@@ -92,51 +93,14 @@ def check_particle_particle_collision(particle_1, particle_2, boundary=100):
     """Given a pair of particles (of equal radius?!) check to see if they have collided. Retuns bool"""
 
     d = cart_distance(particle_1, particle_2) # cartesian distance in 2D
-    cp_x = check_wall_collision(particle_1)
-    cp_x2 = check_wall_collision(particle_2)
     
-    cp_y = check_wall_collision(particle_1, axis='y')
-    cp_y2 = check_wall_collision(particle_2, axis='y')
-    
-    if d<=(particle_1.rad + particle_2.rad - boundary) and cp_x==False and cp_x2==False and cp_y==False and cp_y2==False:
+    if d<=(particle_1.rad + particle_2.rad - boundary):
         return True
     else:
         return False
 
-def stuck_particle(particle_1, particle_2, boundary=100):
-    """Will check if a particle pair is stuck!"""
 
-    _pp_checker = check_particle_particle_collision(particle_1, particle_2, boundary=boundary) # check if they're too close to each other
-    V1 = np.sqrt(particle_1.vx**2 + particle_1.vy**2)
-    V2 = np.sqrt(particle_2.vx**2 + particle_2.vy**2)
-
-    vs = V1==V2
-
-    return _pp_checker and vs
-
-
-def rand_speed_gen(min_speed, max_speed, N=100):
-    """ Returns a list of speeds that do not repeat in components!"""
-    run = True
-    while run:
-        N = 100
-        vx,vy = np.random.randint(min_speed, max_speed, N), np.random.randint(min_speed, max_speed, N)
-        V = np.sqrt(vx**2 + vy**2)
-
-        if len(np.unique(V))==N:
-            break
-        else:
-            u, c = np.unique(V, return_counts=True)
-            dup = u[c > 1] # found duplicated velocities! 
-
-            if len(dup)>0:
-                continue
-            else:
-                run = False
-    return vx, vy
-
-
-def spawn_particles(N0, particle_sigma=300):
+def spawn_particles(N0, vmin=-100, vmax=100, rad=0.011*height, m0=10, seed=42):
     """ This function will spawn N particles in WIN box. It will initialize the position, velocity, mass and size of each particle.
 
     Args:
@@ -144,39 +108,29 @@ def spawn_particles(N0, particle_sigma=300):
         particle_sigma (float): Dispersion in particle position
     """
 
-    # Initialize particle positions
-    #pos_rand_x, pos_rand_y = np.random.normal(width/2, particle_sigma, N0), np.random.normal(width/2, particle_sigma, N0)
+    _step_rec = np.sqrt((width**2)/N0) #reccomended step to evenly space particles
 
-    _step_rec = np.sqrt((width**2)/N0)
-
-    X, Y = [], []
+    X, Y, offset = [], [], 50 # apply some vertical and horizontal offset...
     for i in np.arange(0, width, step=_step_rec):
         for j in np.arange(0, height, step=_step_rec):
             X.append(i)
             Y.append(j)
     pos_rand_x, pos_rand_y = np.array(X), np.array(Y)
-    pos_rand_x, pos_rand_y = pos_rand_x + 25, pos_rand_y + 25
+    pos_rand_x, pos_rand_y = pos_rand_x + offset, pos_rand_y + offset
+    #pos_rand_x, pos_rand_y = np.random.randint(5, width-5, N0), np.random.randint(5, width-5, N0)
 
-    # Initialize particle velocities (assuming uniform distribution)
-    v0x_rand, v0y_rand = rand_speed_gen(-100, 100, N=N0)
-
-    all_particles = [Particle(i, j, ii, jj, 0.01*height, 5) for i, j, ii, jj in zip(pos_rand_x, pos_rand_y, v0x_rand, v0y_rand)]
+    np.random.seed(seed)
+    all_particles = [Particle(i, j, 6*random.choice((-1, 1)), 6*random.choice((-1, 1)), rad, m0) for i, j in zip(pos_rand_x, pos_rand_y)]
 
     return all_particles
 
-
-# initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
-myfont = pygame.font.SysFont("monospace", 15)
-
-
-
-def simulate(N=10, stop_sim=50):
+def simulate(N=10, stop_sim=50, **kwargs):
 
     clock = pygame.time.Clock()
     run = True
     
     # Initialize particles
-    particles_N = spawn_particles(N0=N)
+    particles_N = spawn_particles(N0=N, **kwargs)
     delta_t = 0 
 
     while run:
@@ -192,17 +146,16 @@ def simulate(N=10, stop_sim=50):
             pp.pos_x = pp.pos_x + pp.vx*(1/FPS)
             pp.pos_y = pp.pos_y + pp.vy*(1/FPS)
 
-            
         for ppp in particles_N:
             check_x = check_wall_collision(ppp, axis='x')
             check_y = check_wall_collision(ppp, axis='y')
 
             if check_x==True:
                 ppp.vx = -ppp.vx
-                #ppp.pos_x = ppp.pos_x + pp.vx*(1/FPS)
+                ppp.pos_x = ppp.pos_x + ppp.vx*(1/FPS)
             if check_y==True:
                 ppp.vy = -ppp.vy
-                #ppp.pos_y = ppp.pos_y + pp.vy*(1/FPS)
+                ppp.pos_y = ppp.pos_y + ppp.vy*(1/FPS)
 
 
         # handle particle-particle collisions
@@ -230,7 +183,11 @@ def simulate(N=10, stop_sim=50):
                 v1v2y = pt_1.vy - pt_2.vy
                 v2v1y = pt_2.vy - pt_1.vy
 
-                if _cp==True:
+                if _cp==True and x1x2!=0 and x2x1!=0 and y1y2!=0 and y2y1!=0:
+
+                    ## THE ISSUE IS HERE!!!
+
+
                     pt_1.vx = pt_1.vx - ((2*pt_2.mass)/(m1m2)) * ((np.inner(v1v2,x1x2))/np.linalg.norm(x1x2)**2) * (x1x2)
                     pt_2.vx = pt_2.vx - ((2*pt_1.mass)/(m1m2)) * ((np.inner(v2v1,x2x1))/np.linalg.norm(x2x1)**2) * (x2x1)
 
@@ -250,21 +207,75 @@ def simulate(N=10, stop_sim=50):
 
         k = 1
         for particle in particles_N:
-            #print (f"{k}-ID: x: {particle.pos_x}, y:{particle.pos_y}, Vx:{particle.vx}, Vy:{particle.vy}")
+            print (f"{k}-ID: x: {particle.pos_x}, y:{particle.pos_y}, Vx:{particle.vx}, Vy:{particle.vy}")
             particle.draw_particle(WIN, color=(0, 0, 0))
             k+=1
+
+        # Let's draw a rectangle at the edge of the window display
+        pygame.draw.rect(WIN, (0, 0, 0), (0, 0, 200, 50))
+        _text = time_stamp.render(f"Time: {delta_t}", 1, (255, 0, 0	))
+        WIN.blit(_text, (1, 1))
         
-        tt = time_stamp.render(f"time: {delta_t}", 1, (0,0,0))
-        WIN.blit(tt, (250, 250))
         # Update display!
         update_display()
+
+        # update everything
+        #for particle in particles_N:
+            #particle.pos_x = particle.pos_x
+            #particle.pos_y = particle.pos_y
+            #particle.vx = particle.vx
+            #particle.vy = particle.vy
 
         #print (f"Time interval score: {delta_t} seconds")
         delta_t += 1 
         if delta_t>stop_sim:
             run = False
+            return particles_N
 
 
 # Run the simulation here!
 if __name__=="__main__":
-    ss = simulate(N=100 , stop_sim=5000)
+    ss_1 = simulate(N=100 ,stop_sim=5000, vmin=-150, vmax=150)
+    plt.figure(figsize=(5,5))
+    for p in ss_1:
+        plt.scatter(p.pos_x, p.pos_y, s=50, edgecolor='k')
+        plt.quiver(p.pos_x, p.pos_y, p.vx, p.vy)
+    for _ in range(2):
+        plt.axvline(0, lw=5, color='tomato')
+        plt.axhline(0, lw=5, color='tomato')
+        plt.axvline(1000, lw=5, color='tomato')
+        plt.axhline(1000, lw=5, color='tomato')
+    plt.xlabel("X-pos", fontsize=18)
+    plt.ylabel("Y-pos", fontsize=18)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.xlim(0, 1000)
+    plt.ylim(0, 1000)
+    plt.title("$\delta$t = 10$^{4}$ sec", fontsize=18)
+    plt.savefig('figs/pos_final.jpg', format='jpg', dpi=400, bbox_inches='tight')
+    plt.show()
+
+    np.random.seed(42)
+    #v0x, v0y = np.random.randint(-150, 150, 100), np.random.randint(-150, 150, 100)
+    #V0 = np.sqrt(v0x**2 + v0y**2)
+    v = []
+    for p in ss_1:
+        v.append(np.sqrt(p.vx**2 + p.vy**2))
+    v = np.array(v)
+    
+    plt.figure(figsize=(10,5))
+    _ = plt.hist(v, alpha=0.5, color='tomato', edgecolor='red', lw=3, density=False, histtype='stepfilled', label='Final')
+   #_ = plt.hist(V0, alpha=0.5, color='c', edgecolor='k', bins=np.arange(0, 400, step=25), ls='--', density=True, histtype='stepfilled', label='Inidial')
+    plt.ylabel("N", fontsize=20)
+    plt.xlabel("$v$ [px/s]", fontsize=20)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xticks(fontsize=18)
+    plt.legend(fontsize=18)
+
+    plt.title("$\delta$t = 10$^{4}$ sec", fontsize=18)
+    plt.savefig('figs/dist_final.jpg', format='jpg', dpi=400, bbox_inches='tight')
+    plt.show()
+    
+    
+
